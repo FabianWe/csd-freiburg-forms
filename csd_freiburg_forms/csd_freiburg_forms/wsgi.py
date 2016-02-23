3445a7a6114f44c88e62f55f5b6097e1b636c21b
Fabian Wenzelmann <wenzelmf@informatik.uni-freiburg.de>
2016-02-24 00:37:44 +0100

    initial structure

---
+++ csd_freiburg_forms/csd_freiburg_forms/wsgi.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2016 Fabian Wenzelmann
+#
+# This file is part of csd-freiburg-forms.
+#
+# csd-freiburg-forms is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# csd-freiburg-forms is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with csd-freiburg-forms. If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+WSGI config for csd_freiburg_forms project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "csd_freiburg_forms.settings")
+
+application = get_wsgi_application()
